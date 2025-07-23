e0ebe3cde002bc5bb65bfe248125c9aa3e536d6b
DaanTroost <d.q.troost@outlook.com>
2025-07-23 18:23:43 +0200

    file content :)

--- tests.py
+++ tests.py
@@ -2,9 +2,9 @@ from functions.get_files_info import get_files_info
 from functions.get_files_content import get_files_content
 
 
-# print(get_files_info("calculator", "."))
-# print(get_files_info("calculator", "pkg"))
-# print(get_files_info("calculator", "/bin"))
-# print(get_files_info("calculator", "../"))
 
 print(get_files_content("calculator", "lorem.txt"))
+print(get_files_content("calculator", "main.py"))
+print(get_files_content("calculator", "pkg/calculator.py"))
+print(get_files_content("calculator", "/bin/cat"))
+print(get_files_content("calculator", "pkg/does_not_exist.py"))
